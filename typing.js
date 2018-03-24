83dbbfa0336ecd5b47d058ec5b0eeb6c8048055f
Jai Singhal <jaisinghal48@gmail.com>
2018-03-24 17:51:44 +0530

    new commit

--- typing.js
+++ typing.js
@@ -51,6 +51,9 @@ window.onload = function () {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    css.innerHTML = ".txt-rotate > .wrap {}";
     document.body.appendChild(css);
 };
+
+
+
